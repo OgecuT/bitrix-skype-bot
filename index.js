de3b552ddc46b219ce4e3079d87adaa13f2843ef
OgecuT <devogecut@gmail.com>
2017-10-26 23:30:30 +0300

    update

--- index.js
+++ index.js
@@ -18,5 +18,5 @@ server.post('/api/messages', connector.listen());
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
-	session.send("ВОВКА ВАСЬКА: %s", session.message.text);
+	session.send("VOVA VASIA: %s", session.message.text);
 });
