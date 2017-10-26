80eb1aa6ab1a4cf84e45fed57afeacf0f066e38c
OgecuT <devogecut@gmail.com>
2017-10-26 22:37:37 +0300

    update

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-var express = require('express');
-var app = express();
+var restify = require('restify');
+var builder = require('botbuilder');
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.send({name: 'ascasc'});
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+	console.log('%s listening to %s', server.name, server.url);
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+	appId: process.env.MICROSOFT_APP_ID,
+	appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+// Listen for messages from users
+server.post('/api/messages', connector.listen());
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function (session) {
+	session.send("You said: %s", session.message.text);
 });
