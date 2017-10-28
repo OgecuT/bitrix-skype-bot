67ce4ec7f9c97678c1b9e3000e0b3d1263e93dae
OgecuT <devogecut@gmail.com>
2017-10-28 15:28:34 +0300

    update

--- app/services/botService.js
+++ app/services/botService.js
@@ -7,36 +7,19 @@ const BotConnector = require('./botConnector');
  */
 class BotService {
     constructor() {
-	    this.bot = new builder.UniversalBot(BotConnector.getConnector(), [this.taskHandler]);
+	    this.bot = new builder.UniversalBot(BotConnector.getConnector());
 
-	    this.bot.dialog('/help', function (session, args) {
-	    	console.log('ascasc', args);
-		    session.send("I'm a simple echo bot.");
-		    session.endDialog("It's %s. YOU WIN!", session.message.text);
-	    }).triggerAction({ matches: /^help/i });
-
-	    this.bot.use({
-		    botbuilder: function (session, next) {
-			    if (/^log on/i.test(session.message.text)) {
-				    session.userData.isLogging = true;
-				    session.send('Logging is now turned on');
-			    } else if (/^log off/i.test(session.message.text)) {
-				    session.userData.isLogging = false;
-				    session.send('Logging is now turned off');
-			    } else {
-				    if (session.userData.isLogging) {
-					    console.log('Message Received: ', session.message.text);
-				    }
-				    next();
-			    }
-		    }
-	    });
+	    this.bot
+		    .dialog('/help', [this.taskHandler])
+		    .triggerAction({ matches: /^help/i });
 
 	    this.taskHandler = this.taskHandler.bind(this);
     }
 
-    taskHandler(session) {
-	    session.send("Tell me about it..., %s", session.message.text);
+    taskHandler(session, args) {
+	    console.log('ascasc', args);
+	    session.send("I'm a simple echo bot.");
+	    session.endDialog("It's %s. YOU WIN!", session.message.text);
     }
 
 }
